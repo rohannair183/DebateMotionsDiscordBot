2c0f77872c744f186303d0df2482444b2fd8f4a5
rohannair183 <64338648+rohannair183@users.noreply.github.com>
2021-04-16 23:12:33 -0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+require("dotenv").config();
+
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.login(process.env.TOKEN);
+
+let readMe = fs.readFileSync("data.txt", "utf8").split("\n");
+
+let prefix = process.env.PREFIX;
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("message", (message) => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).trim().split(" ");
+    const command = args.shift().toLowerCase();
+
+    if (command == "getmotion") {
+        let motion = readMe[Math.floor(Math.random() * 6914)];
+        message.channel.send(motion);
+        console.log(motion);
+    }
+});
